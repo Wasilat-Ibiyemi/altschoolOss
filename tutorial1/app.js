eebd61f42636fe89d3da4dfa0f18bc8946289764
Wasilat Ibiyemi <wibiyemi@gmail.com>
2022-08-12 05:21:29 +0100

    mini js

---
+++ tutorial1/app.js
@@ -0,0 +1,4 @@
+let age = prompt("Welcome, Kindly state your age below", '');
+let ageDeclaration = age <=3 ? "We love cute babies":
+age <= 18 ? "welcome dear": age <= 100 ? "Hey papi!": "Old chap!";
+console.log(ageDeclaration);
